eacdffc0245dc5c7566ade1c7938026d27427a0b
etiens <pompichtaru@hotmail.com>
2018-08-02 20:16:09 -0400

    - Tweaked ad for Guildmasters

--- app/components/Footer/index.jsx
+++ app/components/Footer/index.jsx
@@ -9,15 +9,22 @@ export default class PageFooter extends React.Component {
   render() {
     return (
         <footer className="bs-docs-footer" role="contentinfo">
-			<div>				
-				<a href="https://discord.gg/VVb6hDr" target="_blank"><p><b>Follow my next RPG on discord!</b></p></a>
-			</div>
+			<ul className="bs-docs-social-buttons">
+				<li>
+					<div>
+						<p className="footer-outline"><b>Check out my video game!</b></p>
+						<a href="http://store.steampowered.com/app/769410/Electromaze_Tower_Defense/" target="_blank"><div className="center-block EMD-button"/></a>
+					</div>
+				</li>
+				<li>
+					<div>				
+						<p className="footer-outline"><b>Follow my next RPG on discord!</b></p>
+						<a href="https://discord.gg/VVb6hDr" target="_blank"><div className="center-block GM-button"/></a>
+					</div>
+				</li>
+			</ul>
 			<div>
-				<p><b>I made a video game! Feel free to check it out! :)</b></p>
-				<a href="http://store.steampowered.com/app/769410/Electromaze_Tower_Defense/" target="_blank"><div className="center-block EMD-button"/></a>
-			</div>
-			<div>
-				<p><b> Comments, suggestions, crazy hook or trait idea? </b></p>
+				<p className="footer-outline"><b> Comments, suggestions, crazy hook or trait idea? </b></p>
 				<p> Email me at: <a href="mailto:etienspb@gmail.com">EtiensPB@gmail.com</a>!</p>
 			</div>
             <div className="bs-docs-social">
