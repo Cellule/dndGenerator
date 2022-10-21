0d884a6cfefe27720c0c4f5e5692cab8442f323c
Michael Ferris <mike.ferris@hotmail.com>
2022-10-21 09:44:12 -0400

    Revert "Javascript redirect of www.npcgenerator.com to npcgenerator.com"
    This reverts commit ed519b129e223d8ab1c58face022dbfa0868d995.

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,4 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 
-if (process.env.NODE_ENV === "production") {
-  if (window.location.host === "www.npcgenerator.com") {
-    console.log("you are accessing us via www. Redirecting you to non-www.");
-    window.location.href = window.location.href.replace("www.", "");
-  }
-  // if (window.location.protocol === "http:") {
-  //   console.log("you are accessing us via an insecure protocol (HTTP). Redirecting you to HTTPS.");
-  //   window.location.href = window.location.href.replace("http:", "https:");
-  // }
-}
-
 createRoot(document.getElementById("root")!).render(<App />);
