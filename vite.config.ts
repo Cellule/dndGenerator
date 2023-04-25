8dbc552fc97851b8d9a347e006368d1dcdf9e2c5
Michael Ferris <mike.ferris@hotmail.com>
2023-04-25 14:53:27 -0400

    Change dev port back to 3000 and make sure cypress works

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+  },
   resolve: {
     alias: {
       "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
